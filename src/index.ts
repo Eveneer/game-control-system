9bdda98a84af5e854f5ba26246a30744527d4b11
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-13 00:12:39 +0600

    move types to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,13 @@
-export type GameProgressionType = "time-based" | "move-based";
+import type {
+    GameProgressionType,
+    GameModesType,
+    GameMoveType,
+    GameControlSystemPropertiesType,
+    GameControlSystemFunctionsType,
+    GameControlSystemType,
+} from "./types.ts";
 
-export type GameModesType =
-    | "time-limit" // A game mode where the game ends after a limited amount of time
-    | "move-limit" // A game mode where the game ends after a limited numer of moves
-    | "score-rate" // A game mode where a score rate has to be maintained (scores per unit time)
-    | "move-rate"; // A game mode where a move rate has to be maintained (moves per unit time)
-
-export type GameMovesType<T> = Array<T>;
-
-export interface GameMoveType {
-    score: number;
-    moves: number;
-    gameState: any;
-}
-
-export interface GameControlSystemPropertiesType {
-    progression: GameProgressionType;
-    score: number;
-    mode: GameModesType[];
-    timeElapsed: number;
-    movesMade?: number;
-    moves?: GameMovesType<any>;
-    options?: string[];
-    isOptionsVisible?: boolean;
-    speed?: number;
-    isRunning: boolean;
-    hasStarted: boolean;
-    gameStartTime: string | Date | undefined;
-    gameEndTime: string | Date | undefined;
-    winCheckCallback?: () => boolean;
-    loseCheckCallback: () => boolean;
-}
-
-export interface GameControlSystemFunctionsType {
-    startGame: () => void;
-    pauseGame: () => void;
-    unpauseGame: () => void;
-    endGame: () => void;
-    incrementScore: (val: number) => void;
-    decrementScore: (val: number) => void;
-    isPaused: () => boolean;
-    hasWon: () => boolean;
-    hasLost: () => boolean;
-    progressGame: () => void;
-    recordMove: () => void;
-    toggleOptionsVisibility: () => void;
-}
-
-class GameControlSystem implements GameControlSystemPropertiesType {
+class GameControlSystem implements GameControlSystemType {
     progression: GameProgressionType;
     score: number;
     mode: GameModesType[];
@@ -60,7 +20,7 @@ class GameControlSystem implements GameControlSystemPropertiesType {
     movesMade?: number;
     options?: string[];
     isOptionsVisible?: boolean;
-    moves: GameMovesType<any>;
+    moves: GameMoveType[];
     winCheckCallback?: () => boolean;
     loseCheckCallback: () => boolean;
 
---
+++ src/types.ts
@@ -0,0 +1,50 @@
+export type GameProgressionType = "time-based" | "move-based";
+
+export type GameModesType =
+    | "time-limit" // A game mode where the game ends after a limited amount of time
+    | "move-limit" // A game mode where the game ends after a limited numer of moves
+    | "score-rate" // A game mode where a score rate has to be maintained (scores per unit time)
+    | "move-rate"; // A game mode where a move rate has to be maintained (moves per unit time)
+
+export interface GameMoveType {
+    score: number;
+    moves: number;
+    gameState: any;
+}
+
+export interface GameControlSystemPropertiesType {
+    progression: GameProgressionType;
+    score: number;
+    mode: GameModesType[];
+    timeElapsed: number;
+    movesMade?: number;
+    moves?: GameMoveType[];
+    options?: string[];
+    isOptionsVisible?: boolean;
+    speed?: number;
+    isRunning: boolean;
+    hasStarted: boolean;
+    gameStartTime: string | Date | undefined;
+    gameEndTime: string | Date | undefined;
+    winCheckCallback?: () => boolean;
+    loseCheckCallback: () => boolean;
+}
+
+export interface GameControlSystemFunctionsType {
+    startGame: () => void;
+    pauseGame: () => void;
+    unpauseGame: () => void;
+    endGame: () => void;
+    incrementScore: (val: number) => void;
+    decrementScore: (val: number) => void;
+    isPaused: () => boolean;
+    hasWon: () => boolean;
+    hasLost: () => boolean;
+    progressGame: () => void;
+    recordMove: () => void;
+    toggleOptionsVisibility: () => void;
+}
+
+export interface GameControlSystemType
+    extends GameControlSystemPropertiesType,
+        GameControlSystemFunctionsType {}
